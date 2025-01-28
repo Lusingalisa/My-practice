b0e43febbf51224236284043c99425d3799159b7
Namugenyi Lisa Lusinga <lusingalisa7@gmail.com>
2025-01-28 19:05:50 +0300

    Ceasar cipher & Palindrome checker projects

---
+++ PALINDROME.py
@@ -0,0 +1,13 @@
+def is_palindrome(s):
+    # Normalize the string
+    #The  goal is to clean the string in order to only contain lowercase letters and numbers.
+    normalized = ''.join(char.lower() for char in s if char.isalnum())
+    # Check if the original string is equal to the reversed string.
+    return normalized == normalized[::-1]
+
+# Main program
+input_string = input("Enter a string: ")
+if is_palindrome(input_string):
+    print("It's a palindrome.")
+else:
+    print("It's not a palindrome.")

fd31618b48456ff601601575b534b20fc3212398
Namugenyi Lisa Lusinga <lusingalisa7@gmail.com>
2025-02-01 20:18:25 +0300

    Add files via upload

---
+++ basic-if-statements.py
@@ -0,0 +1,73 @@
+#number=int(input("Enter the number: "))
+#if number > 10:
+   # print("number is greater than 10")
+#else:
+   # print("number is less than 10") 
+
+#is_logged_in = False
+# if is_logged_in:
+#     print("welcome")
+# else:
+#    print("You are not logged in") 
+
+# Day =input("Enter the day of the week: ")
+# if Day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]:
+#     print("it is a week day")
+# else:
+#     print("it is not a weekday") 
+
+# correct_password = "friyay"
+# password = input("Enter the password: ")
+# if password == correct_password:
+#     print("Access granted")
+# else:
+#     print("Access denied")
+
+#correct_password= 12345
+#attempt = int(input("Enter the password: "))
+# if attempt == correct_password:
+#     print("Access granted")
+# else:
+#     print("Access denied")
+
+# number = int(input("Enter a number: "))
+# if (number % 2) == 0:
+#     print("the number is even")
+# else:
+#     print("the number is odd")
+
+# letter=input("Enter a letter: ")
+# if letter in ['a','e','i', 'o', 'u']:
+#     print("letter is a vowel")
+# else:
+#     print("letter is not a vowel") 
+
+# number = int(input("Enter a number: "))
+# if (number % 3)==0:
+#     print("the number is divisible by 3")
+# else:
+#     print("the number is not divisible by 3")
+
+#to get the last number that a user has entered
+##e.g if a number ends with 5
+# num=int(input("Enter a number: "))
+# if(num%10==5):
+#    print("the number ends with a five")
+# else:
+#    print("the number does not end with a 5") 
+
+#(a%num ==remainder)
+
+#divisible by 12
+# number=int(input("Enter the number: "))
+# if (number%12 == 0):
+#    print("divisible by 12")
+# else:
+#    print("not divisible by 12") 
+
+# n=5
+# while n>0:
+#    print(n)
+#    n=n-1
+#    print("Blastoff")
+#    print(n)
